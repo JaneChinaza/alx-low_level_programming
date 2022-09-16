dff8cefa4516771fe4c14908a59bad90c7a43e1c
JaneChinaza <janeeke123@gmail.com>
2022-09-15 20:45:31 -0700

    Based on the main.c file above, create a file named 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * ain - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
